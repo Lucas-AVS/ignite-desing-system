180902a41f872852c2f0dff18d0e67ba3a99e463
Ponk8 <lucas.alencar31@gmail.com>
2023-11-15 20:42:39 -0300

    feat: Utilize ArgTypes for type control in components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/Lucas-AVS.png',
     alt: 'Lucas de Alencar',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
